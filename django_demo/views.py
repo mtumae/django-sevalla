f1ca536214cae31c8dcad5a05aca1742c2e99cef
mtume <mtume2016@gmail.com>
2025-10-07 12:47:16 +0300

    test commit

--- django_demo/views.py
+++ django_demo/views.py
@@ -10,3 +10,8 @@ def django_default_view(request):
     path = os.path.join(os.path.dirname(django.__file__), 'views', 'templates', 'default_urlconf.html')
     template = engines['django'].from_string(open(path).read())
     return HttpResponse(template.render({}, request))
+
+
+
+def home(request):
+    return HttpResponse("Hello, Django!")
